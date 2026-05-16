c0fb4cf67c64c5f6a8660fe2e0cbca96dcfa0653
hashiyuta <ktc25a31a0044@edu.kyoto-tech.ac.jp>
2026-05-16 16:07:46 +0900

    課題1

---
+++ Kadai/Kadai1/Kadai1/Kadai1.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+struct Parameter
+{
+    char name[10];
+    int hp;
+    int attack;
+    int defense;
+};
+
+void DispParameter(Parameter parameter)
+{
+    printf("名前 %s\n", parameter.name);
+    printf("HP %d\n", parameter.hp);
+    printf("攻撃力 %d\n", parameter.attack);
+    printf("防御力 %d\n\n", parameter.defense);
+}
+
+int main()
+{
+    Parameter slime_parameter = { "slime", 30, 5, 10 };
+    DispParameter(slime_parameter);
+
+    Parameter goblin_parameter = { "goblin", 100, 30, 50 };
+    DispParameter(goblin_parameter);
+
+    return 0;
+}
